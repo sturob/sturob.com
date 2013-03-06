c366c2b8b2cddb6ec5dc9f685136dbd54ada0a9c
Stuart Robinson <stumrob@gmail.com>
2013-03-06 17:07:06 +0000

    more refactoring

--- experiments/epl2013/main.js
+++ experiments/epl2013/main.js
@@ -1,4 +1,5 @@
-var w = 500,
+
+var w = 600,
     h = 400,
     padding = 0;
 
@@ -6,9 +7,9 @@ var w = 500,
 var championsPoints = 95,
     safetyPoints = 40;
 
-var svg = d3.select("body")
-            .append("svg")
-            .attr("width", w).attr("height", h); 	
+var svg1 = d3.select("body")
+             .append("svg")
+             .attr("width", w).attr("height", h); 	
 
 d3.csv('2012-13.csv', loadCSV);
 
@@ -16,23 +17,22 @@ function loadCSV(matches) {
 	window.d = matches;
 	window.teams = {};
 
-	var max = 120; //d3.max(matches, function(d){ return d.points });
+	var max = 105; //d3.max(matches, function(d){ return d.points });
 	
 	window.x = d3.time.scale()
 	          .domain([ new Date(2012, 7, 16), new Date(2013, 5, 19) ])
 	          .range([ 0, w ]);
 
 	window.y = d3.scale.linear()
-	          .domain([ 0, max ])
-	          .range([h, 0]);
+	          .domain([ 0, max ]) // input
+	          .range([ h, 0 ]);   // maps to
 
 	window.y2 = d3.scale.linear()
 	          .domain([ 0, max ])
 	          .rangeRound([h, 0]);
 
 
-
-	matches.forEach(function(match) {
+	matches.forEach(function populateTeamResults(match) {
 		_.extend(match, Match);
 
 		function saveResult(name) {
@@ -44,7 +44,7 @@ function loadCSV(matches) {
 			t.currentGoalDiff += match.difference(name);
 			t.currentGoalsScored += match.scored(name);
 
-			t.matches.push({
+			t.results.push({
 				date: new Date( match.date() ),
 				points: t.currentPoints,
 				scored: t.currentGoalsScored
@@ -55,76 +55,71 @@ function loadCSV(matches) {
 		saveResult(match.AwayTeam);
 	});
 	
+	// todo do this in d3 natively by mapping {} => []
 	for (t in teams) {
 		graphTeam(teams[t], t)
 	}
 
 	// club titles
-	var tots = _.map(teams, function(t, name){ 
-		return { name: name, currentPoints: t.currentPoints } 
-	});
+	var teamArray = _.map(teams, function(t, name){ return t });
 
-	svg.selectAll("text").
-    data(tots).
-    enter().
-    append("svg:text").
-    attr("x", function(datum, index) { return 400 }).
-    attr("y", function(datum) { return y2(datum.currentPoints); }).
-    attr("class", "team-name").
-    text(function(datum) { return datum.name });
+	svg1.selectAll("text")
+	    .data(teamArray)
+      .enter()
+      .append("svg:text")
+      .attr("x", function(datum, index) { return 400 })
+      .attr("y", function(datum) { return y2(datum.currentPoints); })
+      .attr("class", "team-name")
+      .text(function(datum) { return datum.name });
 
-    // Y axis
-		var yAxis = d3.svg.axis()
-		              .scale(y)
-		              .orient("left")
-		              .ticks(5);
+	// X+Y axis
+	var yAxe = d3.svg.axis();
+	var xAxe = d3.svg.axis();
 
-		svg.append("g")
-		    .attr("class", "axis")
-		    .attr("transform", "translate(" + 30 + ",0)")
-		    .call(yAxis);
+	xAxe.scale(x).ticks(7);
+  yAxe.scale(y).orient("left").ticks(5);
 
-		// X axis
-		var xAxis = d3.svg.axis()
-		              .scale(x)
-		              .ticks(7);
+	svg1.append("g")
+	    .attr("class", "axis")
+	    // .text(function(d, i) { return monthNames[i]; })
+	    .attr("transform", "translate(0," + 0 + ")")
+	    .call(xAxe);
 
-		svg.append("g")
-		    .attr("class", "axis")
-        // .text(function(d, i) { return monthNames[i]; })
-		    .attr("transform", "translate(0," + 0 + ")")
-		    .call(xAxis);
+	svg1.append("g")
+	    .attr("class", "axis")
+	    .attr("transform", "translate(" + 30 + ",0)")
+	    .call(yAxe);
 
 
-		var lineGroup = svg.append("svg:g")
-  			               .attr("transform", "translate("+ padding + ", " + padding + ")");
+	var guidelines = svg1.append("svg:g")
+			                 .attr('class', 'guidelines')
+			                 .attr("transform", "translate("+ padding + ", " + padding + ")");
 
-		lineGroup.append("svg:line")
-		         .attr("x1", 0).attr("y1", d3.round(y(championsPoints)))
-		         .attr("x2", w).attr("y2", d3.round(y(championsPoints)))
-		         .attr("stroke", "lightgray");
+	guidelines.append("svg:line")
+	          .attr("x1", 0).attr("y1", d3.round(y(championsPoints)))
+	          .attr("x2", w).attr("y2", d3.round(y(championsPoints)));
 
-		lineGroup.append("svg:line")
-		         .attr("x1", 0).attr("y1", d3.round(y(safetyPoints)))
-		         .attr("x2", w).attr("y2", d3.round(y(safetyPoints)))
-		         .attr("stroke", "lightgray");
+	guidelines.append("svg:line")
+	          .attr("x1", 0).attr("y1", d3.round(y(safetyPoints)))
+	          .attr("x2", w).attr("y2", d3.round(y(safetyPoints)));
 }
 
 
 function graphTeam(data, team) {
-	var line = d3.svg.line()
-  	  .x(function(d) { return x(d.date); })
+	var line = d3.svg.line();
+
+  line.x(function(d) { return x(d.date); })
     	.y(function(d) { return y(d.points); })
       .interpolate("basis");
 
-  svg.append("path")
-      .datum(data.matches)
-      .on("click", function() {
-      	console.log(team)
-      })
-      .attr("class", "line")
-      .attr('stroke', Kits[team] ? Kits[team].base : '#333')
-      .attr("d", line);
+  svg1.append("path")
+     .datum(data.results)
+     .on("click", function() {
+       console.log(team)
+     })
+     .attr("class", "line")
+     .attr('stroke', Kits[team] ? Kits[team].base : '#333')
+     .attr("d", line);
 
 	// ddd.exit().remove();
 }
@@ -138,7 +133,7 @@ var TeamProto = {};
 
 function Team(name){
 	this.name = name;
-	this.matches = [];
+	this.results = [];
 	this.currentPoints = 0;
 	this.currentGoalDiff = 0;
 	this.currentGoalsScored = 0;
