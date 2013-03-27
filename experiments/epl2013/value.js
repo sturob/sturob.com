46e79678dcc3af9dc57bc196efbb8b604462ddd9
Stuart Robinson <stumrob@gmail.com>
2013-03-27 17:37:03 +0000

    loading data ok, starting redesign of charting

--- experiments/epl2013/value.js
+++ experiments/epl2013/value.js
@@ -1,79 +1,164 @@
+///////////////////
+// Get data bit 
+//////////////////
+
+// if (false) { switchYStat(); initAxis(); }
+
+var data_host = 'http://localhost:6969/football/';
+var yearsRange = _.range(2001, 2010);
+
+var yearAsGet = function (year) {
+	var yearCSV = function (csv) {
+		var yearData = d3.csv.parse( csv );
+		loadSeason( year, yearData );
+	};
+	return $.get(data_host + year + '.csv').done( yearCSV ) 
+}
+
+var requests = yearsRange.map( yearAsGet );
+requests.unshift( $.getJSON( data_host + 'spend-2000-10.json' ) );
+
+// .apply() cos we're passing in an array not args (jquery is stoopid)
+$.when.apply($, requests).then(function (requests, b) {
+	var spendData = requests[0];
+	_(spendData).each(function (club) {
+		_.each(yearsRange, function (year) {
+			var total_wages  = (club['total' + (year - 1)] - 0),
+			    net_transfer = (club['net' + (year - 1)] - 0),
+			    yearly_cost  = total_wages + net_transfer;
+
+			if (isNaN(yearly_cost)) { cost = 0; }
+			if (teams[ club.team ]) {
+				teams[ club.team ].seasons[year].cost = yearly_cost;	
+			}
+		})
+	});
+
+	var c = ['Everton', 'Man City', 'Man United', 'Liverpool', 'Chelsea', 'Tottenham', 'Arsenal'];
+
+	window.data = _.map(c, function (name) {
+		var seasons = _(yearsRange).map(function (year) {
+			var t   = teams[name].seasons[year],
+			    cpp = t.cost / t.currentPoints;
+			return { year:year, cpp:cpp, cost:t.cost, points:t.currentPoints }
+		});
+		return { name:name, seasons:seasons }
+	})
+})
+
+
+
+function loadSeason(year, matches) {
+	matches.forEach(function (match) { 
+		populateTeamResults(match, year);
+	}); // Matches -> Team + Results
+
+	// club titles
+	return;
+
+	window.teamArray = _.chain(teams)
+	                    .map(function (t) { return t })
+	                    .sortBy(function (t) { return t.currentPoints })
+	                    .value();
+
+	window.results = _.map(teamArray, function (t, name) { 
+		return t.results
+	});
+}
+
+function populateTeamResults(match, year) {
+	_.extend(match, Match);
+
+	function saveResult(name) {
+		var t = teams[name];
+		if (! t) {
+			teams[name] = t = new Team(name);
+		}
+
+		var season = t.seasons[year];
+
+		season.currentPoints += match.points(name);
+		season.currentGoalDiff += match.difference(name);
+		season.currentGoalsScored += match.scored(name);
+		season.currentPpg = season.currentPoints / (season.results.length + 1);
+
+		season.results.push({
+			date: new Date( match.date() ),
+			result: match.toString(),
+			ppg: season.currentPpg,
+			goalDiff:  season.currentGoalDiff,
+			points:    season.currentPoints,
+			goalsScored: season.currentGoalsScored
+		});
+	}
+
+	saveResult(match.HomeTeam);
+	saveResult(match.AwayTeam);
+}
+
+
+// --------------------------------------
+// Team Class
+// --------------------------------------
+var Season = {
+	sorted: function() {} // returns array sorted by position
+}
+
+
+
+
+var TeamProto = {
+	setEmptySeasons: function (a, b) {
+		var team = this;
+		team.seasons = {};
+
+		_.each( _.range(a, b), function (year) {
+			team.seasons[year] = {
+				results: [],
+				currentPpg: 0,  currentPoints: 0,
+				currentGoalDiff: 0,  currentGoalsScored: 0
+			};
+		})
+	}
+};
+
+function Team(name){
+	this.name = name;
+	this.kit = Kits[name] ? Kits[name] : Kits.default;
+	this.setEmptySeasons(2000, 2013);
+};
+
+Team.prototype = TeamProto;
+
+
+
+
+////////////////////////////////////////////
+// show data bit
+////////////////////////////////////////////
+
 
 var margin = { top: 20, right: 0, bottom: 30, left: 50 },
-    w = 320 - margin.left - margin.right,
+    w = 600 - margin.left - margin.right,
     h = 600 - margin.top - margin.bottom;
     padding = 0;
 
-// todo - calc averages
-var championsPoints = 95,
-    safetyPoints = 40;
-
 var transTime = 1500;
 
-var period = {
-	end: 2013
-};
-period.start = period.end - 1;
-period.file = period.start + '-' + (period.end - 2000) + '.csv';
-
-function n_to_date (n) {
-	var a = new Date(period.start, 7, 13)
-	var b = new Date(period.end, 5, 19)
+function n_to_date (n, period) {
+	var a = new Date( period.start, 7, 13 )
+	var b = new Date( period.end, 5, 19 )
 	return new Date( a + ((b - a) / n) )
 }
 
 
 
-
-var yearsRange = _.range(2001, 2010);
-
-var yearAsGet = function(year) {
-
-	var yearCSV = function(csv) {
-		var yearData = d3.csv.parse( csv );
-
-		// loadYear()
-
-		// console.log( year )
-		// console.log( yearData )
-	};
-
-	return $.get('http://localhost:6969/football/' + year + '.csv').done( yearCSV ) 
-}
-
-var spendData;
-var spendLoaded = function(json) {
-	spendData = json;
-}
-
-var requests = yearsRange.map( yearAsGet );
-requests.unshift( $.getJSON('http://localhost:6969/football/spend-2000-10.json').done( spendLoaded ) );
-
-// .apply() cos we're passing in an array not args (jquery is stoopid)
-$.when.apply($, requests).then( function(requests, b){ 
-	_(spendData).each(function(club) {
-		console.log('merging spend into club:' + club.team);
-
-		_.each(yearsRange, function(year) {
-			var cost = (club['total' + year] - 0) + (club['net' + year] - 0)
-			console.log( year + ": £" + cost )
-		})
-	  // team
-	});
-
-	// start the real business
-})
-
-
-
-
-
-var winner_data = d3.range(0, 38).map(function(d, n){ 
-	return {
-		points: championsPoints/38 * d,
-		date: n_to_date( d )
-	}
-});
+// var winner_data = d3.range(0, 38).map(function(d, n){ 
+// 	return {
+// 		points: championsPoints/38 * d,
+// 		date: n_to_date( d )
+// 	}
+// });
 
 var cycle = function (arr) {
 	var i = 0;	
@@ -94,7 +179,7 @@ var nextStat = cycle([ 'points', 'ppg', 'goalsScored', 'goalDiff' ]);
 var div = d3.select("body").append("div").attr("class", "tooltip").style("opacity", 0);
 
 var teams = {},
-    x = d3.time.scale(),
+    x = d3.scale.linear(),
   	y = d3.scale.linear();
 
 // X+Y axis
@@ -107,31 +192,33 @@ var svg1 = d3.select("body")
              .attr("height", h + margin.top + margin.bottom)
              .on("click", switchYStat);
 
-d3.csv(period.file, loadCSV);
+
 
 
 // ---------------------------------
 
 function switchYStat() {
-	window.stat = nextStat();
-	document.title = stat;
-	initGraph( stat );
-	update( stat );
-	var t = svg1.transition().duration(transTime);
-	t.select(".y.axis").call( yAxe );
+	// window.stat = nextStat();
+	// document.title = stat;
+	// initGraph( stat );
+	// update( stat );
+	// var t = svg1.transition().duration(transTime);
+	// t.select(".y.axis").call( yAxe );
 }
 
 function initGraph(stat) {
-	var max =  d3.max(teamArray, function(d){ return d[ currentize(stat) ] });
-	var min =  d3.min(teamArray, function(d){ return d[ currentize(stat) ] });
+	// var max =  d3.max(teamArray, function (d){ return d[ currentize(stat) ] });
+	// var min =  d3.min(teamArray, function (d){ return d[ currentize(stat) ] });
 
-	min = min > 0 ? 0 : min; //
-	max = max < 3 ? 3 : max; // hack for ppg
+	// min = min > 0 ? 0 : min; //
+	// max = max < 3 ? 3 : max; // hack for ppg
 
-	x.domain([ new Date(period.start, 7, 13), new Date(period.end, 5, 19) ])
-	 .range([ margin.left, w ]);
-	y.domain([ min, max ]).range([ h - 480, margin.bottom ]);
+	x.domain([ 0, 100 ]).range([ margin.left, w ]);
+	y.domain([ 0, 100 ]).range([ h - 480, margin.bottom ]);
 }
+
+
+initGraph();
 
 
 function initAxis () {
@@ -148,24 +235,6 @@ function initAxis () {
 	    .attr("class", "axis y")
 	    .attr("transform", "translate(" + margin.left + ",0)")
 	    .call(yAxe);
-}
-
-
-function loadCSV(matches) {
-	matches.forEach( populateTeamResults ); // Matches -> Team + Results
-
-	// club titles
-	window.teamArray = _.chain(teams)
-	                    .map(function(t){ return t })
-	                    .sortBy(function(t) { return t.currentPoints })
-	                    .value();
-
-	window.results = _.map(teamArray, function(t, name){ 
-		return t.results
-	});
-
-	switchYStat();
-	initAxis();
 }
 
 
@@ -205,7 +274,7 @@ function update(yStat){
   lines.exit().remove();
 
 
-  window.winners = svg1.selectAll('path.winners-line').data( winner_data );
+  // window.winners = svg1.selectAll('path.winners-line').data( winner_data );
  
  	winners.enter().append('path').attr("d", w_line)
        .attr("transform", function(d, n) {
@@ -286,57 +355,4 @@ function update(yStat){
 }
 
 
-
-function populateTeamResults(match, n) {
-	_.extend(match, Match);
-
-	function saveResult(name) {
-		var t = teams[name];
-		if (! t) {
-			teams[name] = t = new Team(name);
-		}
-
-		t.currentPoints += match.points(name);
-		t.currentGoalDiff += match.difference(name);
-		t.currentGoalsScored += match.scored(name);
-		t.currentPpg = t.currentPoints / (t.results.length + 1);
-
-		t.results.push({
-			date: new Date( match.date() ),
-			result: match.toString(),
-			ppg: t.currentPpg,
-			goalDiff:  t.currentGoalDiff,
-			points:    t.currentPoints,
-			goalsScored: t.currentGoalsScored
-		});
-	}
-
-	saveResult(match.HomeTeam);
-	saveResult(match.AwayTeam);
-}
-
-
-// --------------------------------------
-// Team Class
-// --------------------------------------
-var Season = {
-	sorted: function() {} // returns array sorted by position
-}
-
-
-
-
-var TeamProto = {};
-
-function Team(name){
-	this.name = name;
-	this.kit = Kits[name] ? Kits[name] : Kits.default;
-	this.results = [];
-	this.currentPpg = 0;
-	this.currentPoints = 0;
-	this.currentGoalDiff = 0;
-	this.currentGoalsScored = 0;
-};
-
-Team.prototype = TeamProto;
 
