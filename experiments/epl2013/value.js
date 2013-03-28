e37bc45f24daf1589671768136751eb7d26051c5
Stuart Robinson <stumrob@gmail.com>
2013-03-28 18:00:23 +0000

    show years

--- experiments/epl2013/value.js
+++ experiments/epl2013/value.js
@@ -1,5 +1,5 @@
 
-var margin = { top: 20, right: 0, bottom: 30, left: 50 },
+var margin = { top: 20, right: 0, bottom: 50, left: 50 },
     w = 1200 - margin.left - margin.right,
     h = 600 - margin.top - margin.bottom;
     padding = 0;
@@ -22,13 +22,6 @@ var cycle = function (arr) {
 	}
 }
 
-var interpols = {
-	points: 'step-after',
-	ppg: 'monotone',
-	goalsScored: 'monotone', 
-	goalDiff: 'monotone'
-};
-
 var nextStat = cycle([ 'points', 'ppg', 'goalsScored', 'goalDiff' ]);
 
 var div = d3.select("body").append("div").attr("class", "tooltip").style("opacity", 0);
@@ -48,7 +41,7 @@ var svg1 = d3.select("body")
              .on("click", switchYStat);
 
 
-
+$("body").append('<div id="teams">');
 
 // ---------------------------------
 
@@ -64,6 +57,11 @@ function switchYStat() {
 function dataReady() {
 
 	window.c = _.keys(teams);
+
+	_.each(c, function(t, n) {
+		$('#teams').append( t + " " )
+	})
+
 	window.data = _.map(teams, function (team) {
 		var seasons = _(yearsRange).map(function (year) {
 			var t   = team.seasons[year],
@@ -76,7 +74,7 @@ function dataReady() {
 			return season.points && ! isNaN(season.cost) 
 		})
 
-
+		seasons.team = team; // hacky
 		return seasons // { name:name, seasons:seasons }
 	});
 
@@ -86,10 +84,11 @@ function dataReady() {
 	// min = min > 0 ? 0 : min; //
 	// max = max < 3 ? 3 : max; // hack for ppg
 
-	x.domain([ 10, 300 ]).range([ margin.left, w ]);
-	y.domain([ 18, 95 ]).range([ h, margin.bottom ]);
+	x.domain([ 0, 300 ]).range([ margin.left, w ]);
+	y.domain([ 20, 95 ]).range([ h, margin.bottom ]);
+	
 
-
+	initAxis()
 
 	var line = d3.svg.line()
 	    .interpolate( 'monotone' ).tension(0.99)
@@ -106,44 +105,39 @@ function dataReady() {
 		.append('path').attr("d", line).attr("class", "line").attr('stroke', function(d, n) {
          // return teams[ c[n] ].kit.base
        })
-			.on("click", function(d, n) {
-				console.log( c[n] );
+			.on("click", function(d, n, line) {
+				console.log( d.team.name );
+				$('*.n'+n).toggle();
 				return false;
 			});
 
+	_.each(data, function(team, n) {
+		var dots = svg1.selectAll("text.n" + n).data( team );
 
-	_.each(data, function(team) {
-		var dots = svg1.selectAll("dot").data( team );
-
-		 dots.enter().append("circle")
-		     .attr("r", function(d) { return 2 })
-		     .attr("cx", function(d) { return x(d.cost); })
-		     .attr("cy", function(d) { return y(d.points); })
+		 dots.enter().append("text")
+		 		 .attr('class', 'n'+n)
+		 		 .attr('class', 'year')
+		     // .attr("width", function(d) { return 4 })
+		     // .attr("height", function(d) { return 4 })
+		     .text(function(d){ return (d.year - 1) + "-" + (d.year - 2000) })
+		     .attr("x", function(d) { return x(d.cost) - 12 })
+		     .attr("y", function(d) { return y(d.points) + 4 })
+		     .on("click", function(d, n) {
+			     	console.log( d.year + ": " + d.cost + "m " + d.points );
+			     	return false;
+		     });
 	})
 
 }
 
 
-     // .on("click", function(d, n) {
-    	//  console.log( teamArray[n].name );
-    	//  return false;
-     // })
-     // .attr("transform", function(d, n) {
-     // 		return "translate(0, " + (100 * n) + ')'
-     // })
-     // .attr('stroke-dasharray', function(d, n) { return 8*(n+1) + ',1' })
-
-
-
-
 
 function initAxis () {
-	xAxe.scale(x).ticks(7);
-  yAxe.scale(y).orient("left").ticks(5);
+	xAxe.scale(x).ticks(4);
+  yAxe.scale(y).orient("left").ticks(9);
 	
 	svg1.append("g")
 	    .attr("class", "axis x")
-	    // .text(function(d, i) { return monthNames[i]; })
 	    .attr("transform", "translate(0," + h + ")")
 	    .call(xAxe);
 
@@ -160,83 +154,8 @@ function currentize(stat) {
 
 
 function update(yStat){
-  var line = d3.svg.line()
-      .interpolate( interpols[stat] )
-      .x(function(d) { return x(d.date); })
-      .y(function(d, n) { return y(d[yStat]) });
 
-  var w_line = d3.svg.line()
-      .interpolate( 'linear' )
-      .x(function(d) { return x(d.date); })
-      .y(function(d, n) { return y('points') } );
-
-  window.lines = svg1.selectAll('path.line').data( results );
-
-  lines.enter().append('path').attr("d", line)
-       .on("click", function(d, n) {
-      	 console.log( teamArray[n].name );
-      	 return false;
-       })
-       .attr("transform", function(d, n) {
-       		return "translate(0, " + (100 * n) + ')'
-       })
-       // .attr('stroke-dasharray', function(d, n) { return 8*(n+1) + ',1' })
-       .attr("class", "line")
-       // .attr('stroke', function(d, n) {
-       //   return teamArray[n].kit.base
-       // });
-
-  lines.transition().duration(transTime).attr("d", line);
-  lines.exit().remove();
-
-
-
-
-
-
-  
-
-
-	// var dots = svg1.selectAll("dot").data( results[19] );
-
- //  dots.enter().append("circle")
- //      .attr("r", 1.5)
- //      .attr("cx", function(d) { return x(d.date); })
- //      .attr("cy", function(d) { return y(d[yStat]); })
- //      .on("mouseover", function(d) {      
- //          div.transition().duration(200).style("opacity", .9);
- //          div.html(d.result).style("left", (d3.event.pageX) + "px")
- //                            .style("top", (d3.event.pageY - 28) + "px");
- //          })
- //      .on("mouseout", function(d) {
- //          div.transition().duration(500).style("opacity", 0);
- //      });
-
-	// dots.transition().duration(transTime)
-	//     .attr("cx", function(d) { return x(d.date); })
- //      .attr("cy", function(d) { return y(d[yStat]); });
-
-	// dots.exit().remove();
-
-
-
-  window.short = svg1.selectAll('path.line-2').data( results );
-  short.enter().append('path').attr("d", line)
-       // .attr('stroke-dasharray', function(d, n) { return 8*(n+1) + ',1' })
-       .attr("class", "line-2")
-       .attr("transform", function(d, n) {
-       	 return "translate(0, " + (20 * (20-n)) + ')'
-       })
-
-       // .attr('stroke', function(d, n) {
-       //   return teamArray[n].kit.v
-       // });
-  short.transition().duration(transTime).attr("d", line);
-  short.exit().remove();
-
-
-
-
+ 
 	window.tNames = svg1.selectAll("text").data(teamArray);
 
   var current = currentize(yStat);
