7d09c0f7acde4d14b56b3762e3e61d0b1eed0b36
Stuart Robinson <stumrob@gmail.com>
2013-05-15 02:53:11 +0100

    works with a few bugs

--- tools/maphop/bookmarklet.js
+++ tools/maphop/bookmarklet.js
@@ -1,21 +1,30 @@
 (function () {
+// ⩤ ⩥
+// ⫷maphop⫸
 
-  // Array.map
+// map⫸hop  maphop⫷
+
+// Array.forEach
+
+  function url2keys(url){
+    var b = url.split('?')[1].split('&');
+    var keys = {};
+    b.forEach(function(k){ 
+      var tmp = k.split('=');
+      keys[tmp[0]] = tmp[1];
+    });
+    return keys;
+  }
 
   function A() {
     var pos = { site: '', lat: 0, lng: 0, dir: 0 };
 
     function google() {
       var url = gApplication.getPageUrl();
-      var b = url.split('?')[1].split('&');
-      var keys = {};
-      b.forEach(function(k){ 
-        var tmp = k.split('=');
-        keys[tmp[0]] = tmp[1];
-      });
+      var keys = url2keys(url);
       pos.lat = keys.ll.split(',')[0];
       pos.lng = keys.ll.split(',')[1];
-      pos.dir = keys.cbp.split(',')[1];
+      if (keys.cbp) pos.dir = keys.cbp.split(',')[1];
       pos.site = 'google';
       return pos;
     }
@@ -31,6 +40,8 @@
 
     function bing () {
       pos.site = 'bing';
+      pos.lat = map.get_center().latitude;
+      pos.lng = map.get_center().longitude;
       return pos;
     }
 
@@ -43,21 +54,15 @@
     var sites = {
       google: 'https://maps.google.co.uk/?cbp=12,0,,0,0&ie=UTF8&vpsrc=4&layer=c&ll=' 
                + a.lat + ',' + a.lng + '&spn…=15&cbll=' + a.lat + ',' + a.lng,
-      bing:   'http://www.bing.com/maps/default.aspx?cp=' + a.lat + '~' + a.lng + '&lvl=19&sty=b',
-      nokia:  'http://here.com/'+ a.lat + ',' + a.lng + ',18.8,15.57,68,3d.day'
+      bing:   'http://www.bing.com/maps/default.aspx?cp=' + a.lat + '~' + a.lng + '&lvl=18&sty=b',
+      nokia:  'http://here.com/'+ a.lat + ',' + a.lng + ',18.8,16,68,3d.day'
     }
     window.location = sites[site];
   }
 
 
-  // 
   var CYCLE = { nokia:'google', google:'bing', bing:'nokia' };
 
   var a = A();
   a && B(CYCLE[a.site], a);
-
-  // https://maps.google.co.uk/?ie=UTF8&cbp=12,345.58,,0,0&ll=51.544574,-0.051724&spn=0.002415,0.01074&t=h&z=17&vpsrc=0&layer=c&cbll=51.544206,-0.051439
-  // http://here.com/51.544206,-0.052367,18.8,15.57,68,3d.day        
-  // http://www.bing.com/maps/default.aspx?cp=51.544206~-0.052367&lvl=19&sty=b
-  // http://stackoverflow.com/questions/11104956/is-it-possible-to-pass-data-needed-for-driving-directions-to-the-bing-maps-websi
 })();
