eb6ae6272daa66887c0a7d71fa35c4ecf5b76ac2
Stuart Robinson <stumrob@gmail.com>
2013-05-17 17:56:08 +0100

    rm blimpeye

--- tools/hindenburg/bookmarklet.js
+++ tools/hindenburg/bookmarklet.js
@@ -2,9 +2,10 @@
 // ⩤ ⩥
 // ⫷maphop⫸
 
-// map⫸hop  maphop⫷
-
-// Array.forEach
+  if (! [].forEach) {
+    alert('browser not supported')
+    return;
+  }
 
   function url2keys(url){
     var b = url.split('?')[1].split('&');
@@ -19,7 +20,9 @@
   var pos = { site: '', lat: 0, lng: 0, dir: 0 };
 
   function A() {
-    function google() {
+    function google() { 
+    // // _.VV.lat
+    // window.location.hash.split('!').splice(5, 2).map(function(n){ return n.substr(2) }) -> [ lat, lng ]
       var url = gApplication.getPageUrl();
       var keys = url2keys(url);
       pos.lat = keys.ll.split(',')[0];
@@ -35,6 +38,7 @@
       pos.lng = arr[1];
       pos.dir = arr[3];
       pos.site = 'nokia';
+
       return pos;
     }
 
@@ -42,6 +46,7 @@
       pos.site = 'bing';
       pos.lat = map.get_center().latitude;
       pos.lng = map.get_center().longitude;
+      pos.dir = map.get_heading() ? map.get_heading() : 0;
       return pos;
     }
 
@@ -51,11 +56,23 @@
   }
 
   function B(site, a) {
+    if (site == 'bing') {
+      var offset = a.dir % 90;   
+      if (offset < 45) {           // 220 % 90 -> 40
+        a.dir = a.dir - offset;
+      } else {                     // 230 % 90 -> 50
+        a.dir = a.dir - 0 + (90 - offset);
+      }
+    }
+
+    if (site == 'google' && a.dir == 0) {
+      dir = 0.1;
+    }
     var sites = {
-      google: 'https://maps.google.co.uk/?cbp=12,' + a.dir + ',,0,0&ie=UTF8&vpsrc=4&layer=c&ll=' 
-               + a.lat + ',' + a.lng + '&spn…=15&cbll=' + a.lat + ',' + a.lng,
-      bing:   'http://www.bing.com/maps/default.aspx?cp=' + a.lat + '~' + a.lng + '&lvl=18&sty=b',
-      nokia:  'http://here.com/'+ a.lat + ',' + a.lng + ',17,' + a.dir + ',0,3d.day' // 0->65
+      google: 'http://maps.google.com/?cbp=11,' + a.dir + ',,0,-10&layer=c&ie=UTF8&vpsrc=4&layer=c&ll=' 
+               + a.lat + ',' + a.lng + '&t=h&z=17&spn…=15&cbll=' + a.lat + ',' + a.lng,
+      bing:   'http://www.bing.com/maps/default.aspx?cp=' + a.lat + '~' + a.lng + '&lvl=18&sty=b&dir=' + a.dir,
+      nokia:  'http://here.com/'+ a.lat + ',' + a.lng + ',18,' + a.dir + ',65,3d.day' // 0->65
     }
     return sites[site];
   }
@@ -73,5 +90,11 @@
   var CYCLE = { nokia:'google', google:'bing', bing:'nokia' };
 
   var a = A();
-  if (a) window.location = B(CYCLE[a.site], a);
+  if (! a) {
+    console.log('hindenburg: unsupported site')
+    return;
+  }
+
+  var b = B(CYCLE[a.site], a);
+  if (b) window.location = b;
 })();
