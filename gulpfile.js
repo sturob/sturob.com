6236ef63bbaba1c81c9dbc102d3e436ef1fd37da
Stuart Robinson <stumrob@gmail.com>
2014-11-07 00:54:38 +0000

    trimmed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,6 @@ var htmlhint   = require('gulp-htmlhint');
 var connect    = require('connect');
 var lr         = require('tiny-lr');
 var lrServer   = lr();
-var deploy     = require('gulp-gh-pages');
 
 function httpserver() {
 	return connect().use( require('connect-livereload')() )
@@ -15,15 +14,10 @@ function httpserver() {
 	                .listen('4269');
 }
 
-gulp.task('deploy', function () {
-	var options = {};
-	return gulp.src('./build/**/*')
-	           .pipe(deploy(options));
-});
-
-
-gulp.task('default', function() {
-	console.log('yo, this does nothing')
+gulp.task('base', function() {
+	gulp.src(['./app/**/*', '!./app/assets/', '!./app/*.html'], { base: './app'})
+	    .pipe( gulp.dest('./build') )
+	    .pipe( livereload(lrServer) );
 });
 
 gulp.task('less', function () {
@@ -35,51 +29,12 @@ gulp.task('less', function () {
 });
 
 gulp.task('html', function () {
-	gulp.src( './app/*.html' ) // TODO fix for elsewhere experiments tools wc14 tmp 
+	gulp.src( './app/*.html' )
 	    .pipe( htmlhint() )
 	    .pipe( htmlhint.reporter() )
 	    .pipe( gulp.dest( './build' ) )
 	    .pipe( livereload( lrServer ) );
 });
-
-gulp.task('wc14', function () {
-	gulp.src('./app/wc14/**').pipe( gulp.dest('./build/wc14') )
-	    .pipe( livereload(lrServer) );
-});
-
-gulp.task('tools', function () {
-	gulp.src('./app/tools/**').pipe( gulp.dest('./build/tools') )
-	    .pipe( livereload(lrServer) );
-});
-
-gulp.task('elsewhere', function () {
-	gulp.src('./app/elsewhere/**').pipe( gulp.dest('./build/elsewhere') )
-	    .pipe( livereload(lrServer) );
-});
-
-gulp.task('experiments', function () {
-	gulp.src('./app/experiments/**').pipe( gulp.dest('./build/experiments') )
-	    .pipe( livereload(lrServer) );
-});
-
-gulp.task('tmp', function () {
-	gulp.src('./app/tmp/**').pipe( gulp.dest('./build/tmp') )
-	    .pipe( livereload(lrServer) );
-});
-
-gulp.task('subdirs', [ 'wc14', 'tools', 'elsewhere', 'experiments', 'tmp' ]);
-
-// gulp.task('fonts', function () {
-// 	gulp.src('./app/fonts/**').pipe( gulp.dest('./build/fonts') )
-// 	    .pipe( livereload(lrServer) );
-// });
-
-gulp.task('basics', function () {
-	gulp.src([ 'app/CNAME', 'app/favicon.ico', 'app/_config.yml', 'app/_layouts/**', 'app/_posts/**' ], { base: 'app/' } )
-	    .pipe( gulp.dest('./build/') )
-	    .pipe( livereload(lrServer) );
-});
-
 
 gulp.task('images', function () {
 	gulp.src('./app/assets/images/**').pipe( gulp.dest('./build/assets/images') )
@@ -91,7 +46,8 @@ gulp.task('js', function () {
 	    .pipe( livereload(lrServer) );
 });
 
-gulp.task('build', [ 'basics', 'less', /*'fonts',*/ 'images', 'html', 'js', 'subdirs' ]);
+gulp.task('build', [ 'base', 'less', 'images', 'html', 'js' ]);
+
 gulp.task('server', httpserver);
 
 gulp.task('watch', [ 'build' ], function () {
