c5b3c29d2108fef3ea10f24967e592c74173449e
Stuart Robinson <stumrob@gmail.com>
2014-11-15 21:01:17 +0000

    circle build

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,28 +1,65 @@
 
-
 function setPositions(r, g, b) {
 	function rule (repeat, path, file, x, y) {
-		return "url('" + path + file + "') " + x + ' ' + y + ' ' + repeat
+		return "url('" + path + file + "') " + x + 'px ' + y + 'px ' + repeat
 	}
 
-	var rules = {
-		base: curry(rule, [ 'no-repeat', 'assets/images/' ])
+	// function colorRule (color, x, y) {
+	// 	return 'ba'
+	// }
+
+	var rules = { // universal parameters
+		universal: curry(rule, [ 'no-repeat', 'assets/images/' ])
 	}
 
-	rules.r = curry( rules.base, [ 'me-red.png' ] )
-	rules.g = curry( rules.base, [ 'me-green.png' ] )
-	rules.b = curry( rules.base, [ 'me-blue.png' ] )
+	rules.r = curry( rules.universal, [ 'circle-red.png' ] )
+	rules.g = curry( rules.universal, [ 'circle-green.png' ] )
+	rules.b = curry( rules.universal, [ 'circle-blue.png' ] )
 
 	a.style.background = [
 			rules.r( r[0], r[1] ),
 			rules.g( g[0], g[1] ),
 			rules.b( b[0], b[1] )
-		].join(', ')
+		].join(', ');
+
+
+	// a.style.backgroundSize = '200px'
 }
 
-setPositions(['4px', '7px'], ['-4px', '1px'], ['2px', '-7px'])
+setPositions([4, 7], [-4, 1], [2, -7])
 
-// window.title = 'hello'
+var h = window.innerHeight;
+var w = window.innerWidth;
+
+var scaleX = d3.scale.linear()
+var scaleY = d3.scale.linear()
+
+scaleX.domain([ 0, w ])
+scaleX.range([ w/4 - 100, w/2 + w/4 - 100])
+
+scaleY.domain([ 0, h ])
+scaleY.range([ 0, h * 0.75 ])
+
+// function cycle (n, by) {
+// 	return (n + by) % 10
+// }
+
+window.onmousemove = _.throttle(function (ev) {
+	var x = ev.clientX;
+	var y = ev.clientY;
+
+	var rx = scaleX(x) * 1.1;
+	var ry = scaleY(y) * 1.1;
+
+	var gx = scaleX(x) * 0.8;
+	var gy = scaleY(y) * 1;
+
+	var bx = scaleX(x);
+	var by = scaleY(y) * 0.8;
+
+	setPositions([rx, ry], [gx, gy], [bx, by]);
+}, 1000/60)
+
 
 
 function curry(func,args,space) {
