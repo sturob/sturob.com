5892dbedb6a6a627ec4cea05f6221973e2bbe141
Stuart Robinson <stumrob@gmail.com>
2014-11-15 01:26:46 +0000

    close to new

--- build/assets/js/index.js
+++ build/assets/js/index.js
@@ -1,45 +1,51 @@
 
-// debug.innerHTML = window.innerWidth;
 
-// var BOTH = 1;
-// window.addEventListener('blur', function(){ BOTH-- }, false);
+function setPositions(r, g, b) {
+	function rule (repeat, path, file, x, y) {
+		return "url('" + path + file + "') " + x + ' ' + y + ' ' + repeat
+	}
 
-// function boom() {
-//   $('section#projects').animate( { height: 310 }, 600, 'easeInOutQuad', function() {
-//     $('section#projects div.grid_4, footer').each( function (n,v) { 
-//       setTimeout(
-//         function() { 
-//           $(v).animate({ opacity: 1 }, 200, 'easeInOutQuad', function() {
-//             if (n == 3) {
-//               $('#more_info').delay(400).animate({ opacity: 1 }, 400, 'easeInOutQuad');
-//             }
-//           })
-//         },
-//         (n * 500) + 250
-//       );
-//     });
-//   });
-// }
+	var rules = {
+		base: curry(rule, [ 'no-repeat', 'assets/images/' ])
+	}
 
-// setTimeout( function(){ (++BOTH == 4) && boom(); }, 500 );
-// setTimeout( boom, 5000 ); // if things are taking too long, just do it
+	rules.r = curry( rules.base, [ 'me-red.png' ] )
+	rules.g = curry( rules.base, [ 'me-green.png' ] )
+	rules.b = curry( rules.base, [ 'me-blue.png' ] )
 
-// setTimeout(function() {
-//   $('#typekitjs').next('img').hide()
-// }, 1000);
+	a.style.background = [
+			rules.r( r[0], r[1] ),
+			rules.g( g[0], g[1] ),
+			rules.b( b[0], b[1] )
+		].join(', ')
+}
 
-// $('section#projects img').imagesLoaded(function() {
-//   (++BOTH == 4) && boom();
-// });
-// $('footer').css({ opacity: 0 });
-// $('section#projects div.grid_4').css({ opacity: 0} );
-// $(window).focus(function() {
-//   (++BOTH == 4) && boom();
-// });
-// $(function() {
-//   (++BOTH == 4) && boom();
-//   // window.onkeyup = function(e) {
-//   //   if (e.keyCode == 27) less.watchMode = ! less.watchMode;
-//   //   window.status = 'less refresh ' + (less.watchMode ? 'on' : 'off');
-//   // };
-// });
+setPositions(['4px', '7px'], ['-4px', '1px'], ['2px', '-7px'])
+
+// window.title = 'hello'
+
+
+function curry(func,args,space) {
+	var n  = func.length - args.length; //arguments still to come
+	var sa = Array.prototype.slice.apply(args); // saved accumulator array
+	function accumulator(moreArgs,sa,n) {
+		var saPrev = sa.slice(0); // to reset
+		var nPrev  = n; // to reset
+		for(var i=0;i<moreArgs.length;i++,n--) {
+			sa[sa.length] = moreArgs[i];
+		}
+		if ((n-moreArgs.length)<=0) {
+			var res = func.apply(space,sa);
+			// reset vars, so curried function can be applied to new params.
+			sa = saPrev;
+			n  = nPrev;
+			return res;
+		} else {
+			return function (){
+				// arguments are params, so closure bussiness is avoided.
+				return accumulator(arguments,sa.slice(0),n);
+			}
+		}
+	}
+	return accumulator([],sa,n);
+}
