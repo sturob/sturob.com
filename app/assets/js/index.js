ef2828a356d6a48972759682e0274debddd1a148
Stuart Robinson <stumrob@gmail.com>
2014-11-18 03:48:50 +0000

    lock + grow mechanic

--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -11,13 +11,30 @@ context.globalCompositeOperation = 'lighten';
 var inputs = { }
 
 var dots = {
-	r: [200, 120, 20],
-	g: [190, 120, 20],
-	b: [195, 130, 20]
+	r: [200, 120, 25],
+	g: [190, 120, 25],
+	b: [195, 130, 25]
 }
 
 
+var round = Math.round;
+
+
+function close(x1, x2) {
+	return (x1 + 5 > x2) && (x1 < x2) ||
+	       (x2 + 5 > x1) && (x2 < x1)
+}
+
+function near(a,b,c) {
+
+	return close( a[0], b[0] ) &&
+	       close( a[0], c[0] ) &&
+	       close( a[1], b[1] ) &&
+	       close( a[1], c[1] )
+}
+
 function draw() {
+
 	context.clearRect(0, 0, canvas.width, canvas.height);
 	context
 	  .prop({ fillStyle: '#fcc' }).circle(dots.r[0], dots.r[1], dots.r[2]).fill()
@@ -50,7 +67,7 @@ var scale = {
 }
 
 
-var xdom = [ 0, w ];
+var xdom = [ 10, w ];
 var ydom = [ 0, h ];
 
 
@@ -90,14 +107,14 @@ if (window.location.hash == '#gyro') {
 }
 
 
-scale.r[0].domain( xdom ).range([ w * 0.8,   w - 200 ])
-scale.r[1].domain( ydom ).range([ h * 0.8,   h * 0.8])
+scale.r[0].domain( xdom ).range([ w * 0.84,  w * 0.9 ])
+scale.r[1].domain( ydom ).range([ h * 0.8,   h * 0.85 ])
 
-scale.g[0].domain( xdom ).range([ w * 0.8,   w / 2 ])
-scale.g[1].domain( ydom ).range([ h * 0.8,   h * 1.1  ])
+scale.g[0].domain( xdom ).range([ w * 0.72,  w * .92 ])
+scale.g[1].domain( ydom ).range([ h * 0.8,  h * 0.82  ])
 
-scale.b[0].domain( xdom ).range([ w * 0.8,   w - 100 ])
-scale.b[1].domain( ydom ).range([ h * 0.8,   h - 100  ])
+scale.b[0].domain( xdom ).range([ w * 0.82,  w * 0.9 ])
+scale.b[1].domain( ydom ).range([ h * 0.8,   h * 0.91 ])
 
 
 var updateModel = function (ev) {
@@ -111,14 +128,14 @@ var updateModel = function (ev) {
 	dots.b[0] = scale.b[0](x);
 	dots.b[1] = scale.b[1](y);
 	
+	if (near(dots.r, dots.g, dots.b)) {
+		dots.r[2]++;
+		dots.g[2]++;
+		dots.b[2]++;
+	}
 }
 
 window.onmousemove = _.throttle(updateModel, 1000/60)
-
-
-
-
-
 
 
 
