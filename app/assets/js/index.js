85d192625464d9297bdaa87921d2806e2c17c599
Stuart Robinson <stumrob@gmail.com>
2014-11-18 02:25:56 +0000

    scaling per color + local d3

--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -8,32 +8,31 @@ var context = canvas.getContext('2d');
 context.globalCompositeOperation = 'lighten';
 
 // replace with backbone + skip draw on no change
-var model = {
-	r: [200, 120, 80],
-	g: [190, 120, 80],
-	b: [195, 130, 80]
+var inputs = { }
+
+var dots = {
+	r: [200, 120, 20],
+	g: [190, 120, 20],
+	b: [195, 130, 20]
 }
 
 
 function draw() {
 	context.clearRect(0, 0, canvas.width, canvas.height);
 	context
-	  .prop({ fillStyle: '#00f' }).circle(model.r[0], model.r[1], model.r[2]).fill()
-	  .prop({ fillStyle: '#f00' }).circle(model.g[0], model.g[1], model.g[2]).fill()
-	  .prop({ fillStyle: '#0f0' }).circle(model.b[0], model.b[1], model.b[2]).fill()
+	  .prop({ fillStyle: '#fcc' }).circle(dots.r[0], dots.r[1], dots.r[2]).fill()
+	  .prop({ fillStyle: '#cfc' }).circle(dots.g[0], dots.g[1], dots.g[2]).fill()
+	  .prop({ fillStyle: '#ccf' }).circle(dots.b[0], dots.b[1], dots.b[2]).fill()
 }
 
-	function animate() {
-		requestAnimationFrame( animate )
-		draw()
-	}
- 
+function animate() {
+	requestAnimationFrame( animate )
+	draw()
+}
+
 animate()
 
-
 // data
-
-
 
 var scale = {
 	r: [
@@ -47,70 +46,74 @@ var scale = {
 	b: [
 		d3.scale.linear(),
 		d3.scale.linear()
-	], 
-	x: d3.scale.linear(),
-	y: d3.scale.linear()
+	]
 }
 
 
+var xdom = [ 0, w ];
+var ydom = [ 0, h ];
 
 
+
+// scale.x.range([ 0, w ])
+// scale.y.range([ 0, h ])
 
 // fix this - it currently always returns true on desktop chrome
 // not what we want....
 //window.DeviceOrientationEvent && 
 if (window.location.hash == '#gyro') {
 	// alert('ho')
-	scale.x.domain([ -25, 25 ]) // // gamma -25 -> 25
-	scale.y.domain([ 0, 50 ]) // beta 0 -> 50
 
-	gyro.frequency = 50;
+	xdom = [ -20, 20 ];
+	ydom = [ 10, 40 ];
+
+	gyro.frequency = 1000/60;
 
 	setTimeout(function() {
 		gyro.startTracking(function(o){
 			if (o.gamma) {
-				updateModel({ clientX: o.gamma, clientY: o.beta })
+				updateModel({ 
+					clientX: o.gamma,
+					clientY: o.beta
+				})
+			} else if (o.y) {
+				updateModel({ 
+					clientX: o.gamma,
+					clientY: o.beta
+				})
 			} else {
 				gyro.stopTracking()
-				scale.x.range([ 0, w * 0.75 - 100 ])
-				scale.y.range([ 0, h * 0.75 - 100 ])
 			}
 		})
 	}, 2000)
 } else {
-	scale.x.domain([ 0, w ])
-	scale.y.domain([ 0, h ])
 }
 
-scale.x.range([ 0, w * 0.75 - 100 ])
-scale.y.range([ 0, h * 0.75 - 100 ])
 
+scale.r[0].domain( xdom ).range([ w * 0.8,   w - 200 ])
+scale.r[1].domain( ydom ).range([ h * 0.8,   h * 0.8])
+
+scale.g[0].domain( xdom ).range([ w * 0.8,   w / 2 ])
+scale.g[1].domain( ydom ).range([ h * 0.8,   h * 1.1  ])
+
+scale.b[0].domain( xdom ).range([ w * 0.8,   w - 100 ])
+scale.b[1].domain( ydom ).range([ h * 0.8,   h - 100  ])
 
 
 var updateModel = function (ev) {
 	var x = ev.clientX;
 	var y = ev.clientY;
 
-	var rx = scale.x(x) * 1.1;
-	var ry = scale.y(y) * 1.1;
-
-	var gx = scale.x(x) * 0.8;
-	var gy = scale.y(y) * 1;
-
-	var bx = scale.x(x);
-	var by = scale.y(y) * 0.8;
-
-
-	model.r[0] = rx;
-	model.r[1] = ry;
-	model.g[0] = gx;
-	model.g[1] = gy;
-	model.b[0] = bx;
-	model.b[1] = by;
+	dots.r[0] = scale.r[0](x);
+	dots.r[1] = scale.r[1](y);
+	dots.g[0] = scale.g[0](x);
+	dots.g[1] = scale.g[1](y);
+	dots.b[0] = scale.b[0](x);
+	dots.b[1] = scale.b[1](y);
 	
 }
 
-window.onmousemove = _.throttle(updateModel, 1000/30)
+window.onmousemove = _.throttle(updateModel, 1000/60)
 
 
 
